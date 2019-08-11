3bf58351f2cf8157402b5a54c38b909c17ef6ede
destiny8Star <953857993@qq.com>
2019-08-11 17:57:01 +0800

    11

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,44 @@ Page({
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
-    infos: ["5月11号微点新零售蓝熬枯受淡飞机文熬枯受淡飞机文","卡萨丁九分裤","熬枯受淡飞机文"]
+    infos: ["5月11号微点新零售蓝熬枯受淡飞机文熬枯受淡飞机文","卡萨丁九分裤","熬枯受淡飞机文"],
+    goods:[
+      {
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name:"商品一号id是一号id是一号id是一号id是1一号id是",
+        price:"192",
+        res:"1.2",
+        id:"1"
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        name: "一号adfas一号商品一号商品",
+        price: "192",
+        res: "1.2",
+        id: "2"
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+        name: "商品3号商品3号商品3号商品3号商品3号商品3号商品3号",
+        price: "5632",
+        res: "0.22",
+        id: "3"
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: "商品4号商品4号商品4号商品4号商品4号商品4号商品4号",
+        price: "12342",
+        res: "0.58",
+        id: "4"
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+        name: "商品5号商品5号商品5号商品5号商品5号商品5号商品5号商品5号",
+        price: "1922",
+        res: "2",
+        id: "5"
+      },
+    ]
   },
   //事件处理函数
 
